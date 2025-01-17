47d798f851620b73d8a75a2612f1d03a48cb4121
rajcr12051998 <rajcr12051998@gmail.com>
2025-01-17 13:04:01 +0530

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SharePoint connection details
+export TENANT_ID="<tenant_id>"
+export CLIENT_ID="<client_id>"
+export CLIENT_SECRET="<client_secret>"
+export SHAREPOINT_SITE_URL="https://<tenant>.sharepoint.com/sites/<site-name>"
+export DOCUMENT_LIBRARY="TemplatesLibrary"
+export LOCAL_TEMPLATES_PATH="./templates"  # Local templates directory path
